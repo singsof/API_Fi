ab993d9d40255732b8db9a45f9c424ce01e934f7
SOMPHOL WILA <singsoft.sw@gmail.com>
2022-10-23 15:17:31 +0700

    ก

--- config/connectdb.php
+++ config/connectdb.php
@@ -2,14 +2,10 @@
 date_default_timezone_set('Asia/Bangkok');
 define("DB_TYPE", "MySQL"); // MySQL & SQLite
 define("DB_HOST", "localhost");
-// define("DB_USERNAME", "id19745545_api_fishtank_sw");
-// define("DB_PASSWORD", "FCNMR6i[ukQH}}aM");
-// define("DB_NAME", "id19745545_api_fishtank");  //id19745545_api_fishtank
 
-
-define("DB_USERNAME", "root"); //id19745545_api_fishtank_sw
-define("DB_PASSWORD", ""); //FCNMR6i[ukQH}}aM
-define("DB_NAME", "fish");  //id19745545_api_fishtank
+define("DB_USERNAME", "root"); 
+define("DB_PASSWORD", ""); 
+define("DB_NAME", "fish");  
 
 define("DB_DNS_MYSQL", "mysql:host=" . DB_HOST . "; dbname=" . DB_NAME);
 define("DB_DNS_SQLITE", "sqlite:db/sqlite_file");
