4b97940de01ab22e1dec6d663eae3f343b6f1700
SOMPHOL WILA <singsoft.sw@gmail.com>
2022-10-23 20:57:57 +0700

    xcv

--- index.php
+++ index.php
@@ -21,21 +21,62 @@ include("./config/connectdb.php");
 
 
     <link rel="stylesheet" href="./bootstrap.css">
-    <script src="./jquery-3.6.1.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="./dataTable/datatables.css" />
-    <script type="text/javascript" src="./dataTable/datatables.js"></script>
-    <script type="text/javascript" src="./dataTable/vfs_fonts.js"></script>
 </head>
 
 <body>
 
+    <?php
+
+    $key = isset($_GET["key"]) ? $_GET["key"] : null;
+
+    if ($key == "lGE85ehip") {
+        $start = isset($_GET["start"]) ? $_GET["start"] : null;
+        $end = isset($_GET["end"]) ? $_GET["end"] : null;
+
+        $sql_search = "SELECT *,
+                            DATE_FORMAT(rt.day, '%d') as Dd ,
+                            DATE_FORMAT(rt.day, '%c') as month ,
+                            DATE_FORMAT(rt.day, '%Y') as year, 
+                            DATE_FORMAT(rt.day, '%H : %i ') as time
+                        FROM `esp8266` as rt INNER JOIN ir  ON ir.id_ir = rt.id_ir WHERE  rt.day BETWEEN '{$start}' AND '{$end}' ";
+    } else {
+
+        $sql_search = "SELECT *,
+                            DATE_FORMAT(rt.day, '%d') as Dd ,
+                            DATE_FORMAT(rt.day, '%c') as month ,
+                            DATE_FORMAT(rt.day, '%Y') as year, 
+                            DATE_FORMAT(rt.day, '%H : %i ') as time
+                        FROM `esp8266` as rt INNER JOIN ir  ON ir.id_ir = rt.id_ir";
+    }
+    ?>
+
     <div class="container" style="margin-top: 100px;">
         <h1>ดึงข้อมูลจากฐานข้อมูล มาแสดงเว็บ</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <h4 class="control-label">วันที่เริ่มต้น</h4>
+            </div>
+            <div class="col-md-6">
+                <h4 class="control-label">ถึงวันที่</h4>
+            </div>
+        </div>
+        <form name="form-date" action="./index.php" method="GET">
+            <div class="input-group mb-3">
+
+                <input type="date" class="form-control" name="start" value="<?php echo $key != null ? $start :  "" ?>" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
+                <input type="date" class="form-control" name="end" value="<?php echo $key != null? $end :  date("Y-m-d") ?>" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" name="key" value="lGE85ehip" type="submit">ค้นหา</button>
+                </div>
+
+
+
+            </div>
+        </form>
         <table id="mg_table" class="table  table-borderless " style="width:100%">
             <thead>
                 <tr>
                     <th class="text-center">ลำดับ</th>
-                    <th class="text-center">รหัส ID</th>
                     <th class="text-center">สถานะ</th>
                     <th class="text-center">ค่า PH</th>
                     <th class="text-center">วันที่</th>
@@ -43,15 +84,14 @@ include("./config/connectdb.php");
             </thead>
             <tbody>
                 <?php
+
+
+
+
                 $thaiweek = array("วันอาทิตย์", "วันจันทร์", "วันอังคาร", "วันพุธ", "วันพฤหัส", "วันศุกร์", "วันเสาร์");
                 $thaimonth = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
 
-                $sql_search = "SELECT *,
-                                                    DATE_FORMAT(rt.day, '%d') as Dd ,
-                                                    DATE_FORMAT(rt.day, '%c') as month ,
-                                                    DATE_FORMAT(rt.day, '%Y') as year, 
-                                                    DATE_FORMAT(rt.day, '%H : %i ') as time
-                                            FROM `esp8266` as rt ;";
+
                 $i_r = 1;
                 foreach (DB::query($sql_search, PDO::FETCH_OBJ) as $row) :
                     // $date = 'วันที่ '.$row->Dd.' เดือน'.$thaimonth[$row->month-1].' พ.ศ.'.$row->year+543;
@@ -60,18 +100,17 @@ include("./config/connectdb.php");
                 ?>
                     <tr>
                         <td class="text-center"><?php echo $i_r++ ?></td>
-                        <td class="text-center">PH<?php echo $row->id ?></td>
                         <td class="text-center"><?php echo $row->ir == 0 ? "เติมอาหารแล้ว" : "เติมอาหารแล้ว" ?></td>
                         <td class="text-center">
                             <?php
 
-                                if($row->pH > 7 ){
-                                    echo "เบสหรือด่าง";
-                                }elseif($row->pH == 7){
-                                    echo "เป็นกลาง";
-                                }else{
-                                    echo "กรด";
-                                }
+                            if ($row->pH > 7) {
+                                echo "เบสหรือด่าง";
+                            } elseif ($row->pH == 7) {
+                                echo "เป็นกลาง";
+                            } else {
+                                echo "กรด";
+                            }
 
                             ?>
                         </td>
@@ -89,56 +128,6 @@ include("./config/connectdb.php");
     </div>
 
 
-
-
-    <script>
-        function update_mg_table() {
-            $('#mg_table').DataTable({
-                dom: 'lBfrtip',
-                // select: true,
-                lengthMenu: [
-                    [10, 25, 50, 60, -1],
-                    [10, 25, 50, 60, "All"]
-                ],
-                language: {
-                    sProcessing: "กำลังดำเนินการ...",
-                    sLengthMenu: "แสดง  _MENU_  แถว",
-                    sZeroRecords: "ไม่พบข้อมูล",
-                    sInfo: "แสดง _START_ ถึง _END_ จาก _TOTAL_ แถว",
-                    sInfoEmpty: "แสดง 0 ถึง 0 จาก 0 แถว",
-                    sInfoFiltered: "(กรองข้อมูล _MAX_ ทุกแถว)",
-                    sInfoPostFix: "",
-                    sSearch: "ค้นหา: ",
-                    sUrl: "",
-                    oPaginate: {
-                        "sFirst": "เริ่มต้น",
-                        "sPrevious": "ก่อนหน้า",
-                        "sNext": "ถัดไป",
-                        "sLast": "สุดท้าย"
-                    }
-                }, // sInfoEmpty: "แสดง 0 ถึง 0 ของ 0 เร็คคอร์ด",
-                processing: true, // แสดงข้อความกำลังดำเนินการ กรณีข้อมูลมีมากๆ จะสังเกตเห็นง่าย
-                //serverSide: true, // ใช้งานในโหมด Server-side processing
-                // กำหนดให้ไม่ต้องการส่งการเรียงข้อมูลค่าเริ่มต้น จะใช้ค่าเริ่มต้นตามค่าที่กำหนดในไฟล์ php
-                retrieve: true,
-                buttons: [{
-                    extend: 'print',
-                    text: 'พิมพ์',
-                    messageTop: '',
-
-                    filename: function() {
-                        return "ประวัติการจอง "; //+hour+'-'+minutes + '-'+days +'-'+month +'-'+years
-                    },
-                    // title: 'รายชื่อสิทเข้าห้อง',
-                    exportOptions: {
-                        pageSize: 'LEGAL'
-                    }
-                }]
-            });
-        }
-
-        update_mg_table();
-    </script>
 </body>
 
 </html>
