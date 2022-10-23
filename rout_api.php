f6d0cd00317ca339b6016740a5289f395d139faf
SOMPHOL WILA <singsoft.sw@gmail.com>
2022-10-23 21:00:07 +0700

    ิ

--- rout_api.php
+++ rout_api.php
@@ -10,13 +10,12 @@ if (isset($key) && $key === "tPmAT5Ab3j7F9") {
     $pH = isset($_GET["pH"]) ? $_GET["pH"] : null;
 
     $stm = DB::prepare("INSERT INTO `ir` (`id_ir`, `ir`, `date_ir`) VALUES (NULL, '$IR', current_timestamp());");
-
     $id_ir = null;
     if($stm->execute()){
         // DB::getLink()->commit();
         $id_ir = DB::getLink()->lastInsertId();
     }
- 
+
     if (DB::query("INSERT INTO `esp8266` (`id`, `id_ir`, `pH`, `day`) VALUES (NULL, '$id_ir', '$pH', current_timestamp());")) {
         echo "success";
     } else {
